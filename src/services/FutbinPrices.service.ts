8b13c1c64b02e8431d1336724b40a785c1f5a8c6
mabenj <markobenj@hotmail.com>
2022-02-01 11:38:34 +0200

    Better error handling when fetching price data

--- src/services/FutbinPrices.service.ts
+++ src/services/FutbinPrices.service.ts
@@ -1,40 +1,50 @@
 import axios from "axios";
 import IPriceInfo from "../interfaces/PriceInfo.interface";
+import { sleep } from "../util/utils";
 
+const FETCH_ATTEMPTS = 5;
+const COOLDOWN_MS = 1000;
 const FUTBIN_URL = "https://www.futbin.com/stc/cheapest";
 
 export const fetchFutbinPrices = async (): Promise<[IPriceInfo, string]> => {
 	const prices: IPriceInfo = {};
 	let errorMessage = "";
-	try {
-		const html = (await axios.get<string>(FUTBIN_URL)).data;
-		const parser = new DOMParser();
-		const doc = parser.parseFromString(html, "text/html");
+	let attempts = 0;
+	while (attempts < FETCH_ATTEMPTS) {
+		try {
+			const html = (await axios.get<string>(FUTBIN_URL)).data;
+			const parser = new DOMParser();
+			const doc = parser.parseFromString(html, "text/html");
 
-		const ratingGroups = doc.querySelectorAll(".top-stc-players-col");
+			const ratingGroups = doc.querySelectorAll(".top-stc-players-col");
 
-		for (let i = 0; i < ratingGroups.length; i++) {
-			const rating = Number(
-				ratingGroups[i]
-					.querySelector(".top-players-stc-title>span>span")
-					?.innerHTML.trim()
-			);
-			const playerPrices: number[] = Array.from(
-				ratingGroups[i].querySelectorAll(".price-holder-row>span")
-			).map((span) => parsePrice(span.textContent?.trim()));
+			for (let i = 0; i < ratingGroups.length; i++) {
+				const rating = Number(
+					ratingGroups[i]
+						.querySelector(".top-players-stc-title>span>span")
+						?.innerHTML.trim()
+				);
+				const playerPrices: number[] = Array.from(
+					ratingGroups[i].querySelectorAll(".price-holder-row>span")
+				).map((span) => parsePrice(span.textContent?.trim()));
 
-			if (rating) {
-				prices[rating] = Math.min(...playerPrices);
+				if (rating) {
+					prices[rating] = Math.min(...playerPrices);
+				}
 			}
-		}
-	} catch (error: unknown) {
-		console.warn("Could not fetch player prices: ", error);
-		if (typeof error === "string") {
-			errorMessage = error;
-		} else if (error instanceof Error) {
-			errorMessage = error.message;
-		} else {
-			errorMessage = "Unknown error";
+			errorMessage = "";
+			break;
+		} catch (error: unknown) {
+			console.warn("Could not fetch player prices: ", error);
+			if (typeof error === "string") {
+				errorMessage = error;
+			} else if (error instanceof Error) {
+				errorMessage = error.message;
+			} else {
+				errorMessage = "Unknown error";
+			}
+			attempts++;
+			await sleep(COOLDOWN_MS);
 		}
 	}
 	return [prices, errorMessage];
--- src/util/utils.ts
+++ src/util/utils.ts
@@ -47,3 +47,7 @@ export function calculatePrice(
 	});
 	return sum;
 }
+
+export function sleep(ms: number) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
