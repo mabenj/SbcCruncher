56d888e1ebbf1df86c34775afa9df2603fdc84fa
mabenj <markobenj@hotmail.com>
2022-02-12 17:11:45 +0200

    WIP multirating select

--- src/components/MultiRatingSelect.tsx
+++ src/components/MultiRatingSelect.tsx
@@ -14,6 +14,11 @@ import {
 import "../styles/MultiRatingSelect.scss";
 import Config from "../Config";
 
+interface IOption {
+	rating: string;
+	index: number;
+}
+
 interface IMultiRatingSelectProps {
 	value: number[] | undefined;
 	onChange: (newValue: number[]) => void;
@@ -23,43 +28,52 @@ export const MultiRatingSelect = ({
 	value,
 	onChange
 }: IMultiRatingSelectProps) => {
+	const [originalOptions, setOriginalOptions] = useState<IOption[]>(
+		Config.allRatings.map((r, i) => ({ rating: r.toString(), index: i }))
+	);
+	const [suggestions, setSuggestions] = useState(originalOptions);
 	const [ratingCounts, setRatingCounts] = useState<{ [key: number]: number }>(
 		{}
 	);
-	const [formattedValues, setFormattedValues] = useState<number[]>();
+	// const [formattedValues, setFormattedValues] = useState<number[]>();
 
-	useEffect(() => {
-		if (!value) {
+	// useEffect(() => {
+	// 	if (!value) {
+	// 		return;
+	// 	}
+	// 	const counts: { [key: number]: number } = {};
+	// 	for (const rating of value) {
+	// 		counts[rating] = counts[rating] ? counts[rating] + 1 : 1;
+	// 	}
+	// 	setRatingCounts(counts);
+	// 	setFormattedValues(Object.keys(counts).map((rating) => Number(rating)));
+	// }, [value]);
+
+	const formattedValues: IOption[] | undefined =
+		value?.map((val, i) => ({ rating: val.toString(), index: i })) || [];
+
+	const searchOptions = (e: AutoCompleteCompleteMethodParams) => {
+		const filtered = originalOptions.filter((opt) =>
+			opt.rating.startsWith(e.query)
+		);
+		setSuggestions(filtered);
+	};
+
+	const handleSelect = (e: AutoCompleteChangeParams) => {
+		if ((value?.length || 0) >= Config.playersInSquad) {
 			return;
 		}
-		const counts: { [key: number]: number } = {};
-		for (const rating of value) {
-			counts[rating] = counts[rating] ? counts[rating] + 1 : 1;
-		}
-		setRatingCounts(counts);
-		setFormattedValues(Object.keys(counts).map((rating) => Number(rating)));
-	}, [value]);
+		console.log("select", e);
+		onChange([...(value || []), Number(e.value.rating)]);
+	};
 
-	// const formattedValues: IOption[] | undefined =
-	// 	value?.map((val, i) => ({ rating: val.toString(), index: i })) || [];
-
-	// const searchOptions = (e: AutoCompleteCompleteMethodParams) => {
-	// 	const filtered = RATINGS.filter((opt) => opt.rating.startsWith(e.query));
-	// 	setOptions(filtered);
-	// };
-
-	// const handleSelect = (e: AutoCompleteChangeParams) => {
-	// 	console.log("select", e);
-	// 	onChange([...(value || []), Number(e.value.rating)]);
-	// };
-
-	// const handleUnselect = (e: AutoCompleteChangeParams) => {
-	// 	const newValues = formattedValues.filter(
-	// 		(val) => val.index !== e.value.index
-	// 	);
-	// 	console.log("unselect", { oldValues: formattedValues, newValues, e });
-	// 	onChange(newValues.map((nv) => Number(nv.rating)));
-	// };
+	const handleUnselect = (e: AutoCompleteChangeParams) => {
+		const newValues = formattedValues.filter(
+			(val) => val.index !== e.value.index
+		);
+		console.log("unselect", { oldValues: formattedValues, newValues, e });
+		onChange(newValues.map((nv) => Number(nv.rating)));
+	};
 
 	const handleChange = (newValue: number[] | null) => {
 		if (newValue === null) {
@@ -122,29 +136,32 @@ export const MultiRatingSelect = ({
 	};
 
 	return (
-		<MultiSelect
-			value={formattedValues}
-			options={Config.allRatings}
-			onChange={(e) => handleChange(e.value)}
-			optionLabel="rating"
-			itemTemplate={itemTemplate}
-			selectedItemTemplate={selectedItemTemplate}
-			panelFooterTemplate={footerTemplate}
-			scrollHeight="400px"
-			showClear
-			showSelectAll={false}
-		/>
-		// <AutoComplete
-		// 	multiple
-		// 	forceSelection
-		// 	dropdown
-		// 	field="rating"
-		// 	value={formattedValues.length > 0 ? formattedValues : undefined}
-		// 	suggestions={options}
-		// 	completeMethod={searchOptions}
-		// 	onSelect={handleSelect}
-		// 	onUnselect={handleUnselect}
+		// <MultiSelect
+		// 	value={formattedValues}
+		// 	options={Config.allRatings}
+		// 	onChange={(e) => handleChange(e.value)}
+		// 	optionLabel="rating"
+		// 	itemTemplate={itemTemplate}
+		// 	selectedItemTemplate={selectedItemTemplate}
+		// 	panelFooterTemplate={footerTemplate}
 		// 	scrollHeight="400px"
+		// 	showClear
+		// 	showSelectAll={false}
 		// />
+		<AutoComplete
+			multiple
+			forceSelection
+			dropdown
+			dropdownMode="current"
+			field="rating"
+			value={formattedValues.length > 0 ? formattedValues : undefined}
+			suggestions={
+				(value?.length || 0) >= Config.playersInSquad ? undefined : suggestions
+			}
+			completeMethod={searchOptions}
+			onSelect={handleSelect}
+			onUnselect={handleUnselect}
+			scrollHeight="400px"
+		/>
 	);
 };
