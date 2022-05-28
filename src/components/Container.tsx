c3643d67077da3a98752dfbcd7b1175b4ba8200b
mabenj <markobenj@hotmail.com>
2022-05-28 15:17:15 +0300

    Better scrollbar

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,12 @@
+import { ScrollPanel } from "primereact/scrollpanel";
 import React from "react";
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
     return (
-        <main className="main-container">
-            <div className="children">{children}</div>
-        </main>
+        <ScrollPanel style={{ width: "100vw", height: "100vh" }} className="custom-scroll-panel">
+            <main className="main-container">
+                <div className="children">{children}</div>
+            </main>
+        </ScrollPanel>
     );
 };
