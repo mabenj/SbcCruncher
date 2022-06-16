70d4be022b2cf4799556c5377246f2dd61185456
mabenj <markobenj@hotmail.com>
2022-06-17 00:17:48 +0300

    Fix fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
 import "primereact/resources/primereact.min.css";
+import "../styles/fonts.css";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
