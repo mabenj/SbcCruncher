0143127a0b8b986402a92c9b3cf82655fde5a0ae
mabenj <markobenj@hotmail.com>
2023-02-17 21:06:17 +0200

    Move rating price logic into a hook

--- src/components/form/PlayerPrices.tsx
+++ src/components/form/PlayerPrices.tsx
@@ -1,16 +1,6 @@
-import { EMPTY_PRICES } from "@/constants";
 import { useConfig } from "@/context/ConfigContext";
-import { FutbinParser } from "@/external-prices/FutbinParser";
-import { FutwizParser } from "@/external-prices/FutwizParser";
-import { useEventTracker } from "@/hooks/useEventTracker";
-import useLocalStorage from "@/hooks/useLocalStorage";
-import {
-    capitalize,
-    getErrorMessage,
-    range,
-    sleep,
-    timeAgo
-} from "@/utilities";
+import usePlayerPrices from "@/hooks/usePlayerPrices";
+import { range, timeAgo } from "@/utilities";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import {
     Alert,
@@ -30,137 +20,30 @@ import {
     MenuItemOption,
     MenuList,
     MenuOptionGroup,
-    SimpleGrid,
-    useToast
+    SimpleGrid
 } from "@chakra-ui/react";
 import { mdiClose, mdiDesktopTowerMonitor, mdiGamepadVariant } from "@mdi/js";
 import Icon from "@mdi/react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import HoverTooltip from "../ui/HoverTooltip";
 
-const PRICE_FETCH_MAX_ATTEMPTS = 3;
-const PRICE_FETCH_COOLDOWN_MS = 2_000;
-const EVENT_DEBOUNCE_MS = 3000;
-const PRICE_MAP_OLD_THRESHOLD_MS = 300_000;
-
-const URLS = {
-    futbin: "https://www.futbin.com/stc/cheapest",
-    futwiz: "/api/futwiz-cheapest"
-};
-
-const STORAGE_KEYS = {
-    platform: "prices.platform",
-    dataSource: "prices.dataSource",
-    priceMap: "prices.current",
-    cache: "prices.cache"
-};
-
-const PARSERS = {
-    futbin: FutbinParser.fromHtml,
-    futwiz: FutwizParser.fromHtml
-};
-
-type DataSource = "futbin" | "futwiz";
-type Platform = "console" | "pc";
-
-interface StoredPrices {
-    priceMap: Record<number, number>;
-    timestamp: number;
-}
+const PRICES_AGE_WARN_THRESHOLD_MS = 300_000; // 5m
 
 export default function PlayerPrices() {
     const [config, setConfig] = useConfig();
-    const [pricesLastModified, setPricesLastModified] = useState(-1);
-    const [isFetchingPrices, setIsFetchingPrices] = useState(false);
-    const [dataSource, setDataSource] = useLocalStorage(
-        STORAGE_KEYS.dataSource,
-        "futwiz" as DataSource
-    );
-    const [platform, setPlatform] = useLocalStorage(
-        STORAGE_KEYS.platform,
-        "console" as Platform
-    );
-
-    const toast = useToast();
-
-    const eventTracker = useEventTracker("Prices", EVENT_DEBOUNCE_MS);
+    const prices = usePlayerPrices();
 
     const ratingRange = range(
         Math.min(...config.tryRatingMinMax),
         Math.max(...config.tryRatingMinMax)
     );
 
-    useEffect(() => {
-        const storedPricesJson = localStorage.getItem(STORAGE_KEYS.priceMap);
-        if (storedPricesJson) {
-            const storedPrices = JSON.parse(storedPricesJson) as StoredPrices;
-            setAllPrices(storedPrices.priceMap, storedPrices.timestamp);
-            setPricesLastModified(storedPrices.timestamp);
-        }
+    useEffect(
+        () =>
+            setConfig((prev) => ({ ...prev, ratingPriceMap: prices.priceMap })),
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    const setSinglePrice = (rating: number, price: string) => {
-        let priceNum = +price;
-        priceNum =
-            isNaN(priceNum) || typeof priceNum !== "number" ? 0 : priceNum;
-        config.ratingPriceMap[rating] = priceNum;
-        setAllPrices(config.ratingPriceMap, Date.now());
-        eventTracker("price_set_single=" + rating + "x" + price);
-    };
-
-    const setAllPrices = (
-        prices: Record<number, number>,
-        timestamp: number
-    ) => {
-        setPricesLastModified(timestamp);
-        setConfig((prev) => {
-            const prevPrices = prev.ratingPriceMap;
-            const newPrices = { ...prevPrices, ...prices };
-            const storedPrices: StoredPrices = {
-                priceMap: newPrices,
-                timestamp
-            };
-            localStorage.setItem(
-                STORAGE_KEYS.priceMap,
-                JSON.stringify(storedPrices)
-            );
-            return { ...prev, ratingPriceMap: newPrices };
-        });
-    };
-
-    const fetchPrices = async () => {
-        setIsFetchingPrices(true);
-        try {
-            const prices = await fetchExternalPrices(dataSource, platform);
-            const priceCount = Object.keys(prices).length;
-            setAllPrices({ ...EMPTY_PRICES, ...prices }, Date.now());
-            toast({
-                status: "success",
-                description: `Fetched prices for ${priceCount} ratings`
-            });
-            eventTracker(
-                `price_fetch_ok=${dataSource}-${platform}x${priceCount}`
-            );
-        } catch (error) {
-            toast({
-                status: "error",
-                title: "Could not fetch price data",
-                description: "Wait for a few minutes and try again"
-            });
-            eventTracker(
-                `price_fetch_error=${dataSource}-${platform}`,
-                getErrorMessage(error)
-            );
-        } finally {
-            setIsFetchingPrices(false);
-        }
-    };
-
-    const clearAllPrices = () => {
-        setAllPrices({ ...EMPTY_PRICES }, Date.now());
-        eventTracker("price_clear_all");
-    };
+        [prices.priceMap]
+    );
 
     return (
         <>
@@ -179,7 +62,7 @@ export default function PlayerPrices() {
                             pattern="[0-9]"
                             value={config.ratingPriceMap[rating] || ""}
                             onChange={(e) =>
-                                setSinglePrice(rating, e.target.value)
+                                prices.setPrice(rating, e.target.value)
                             }
                             placeholder="0"
                         />
@@ -187,15 +70,15 @@ export default function PlayerPrices() {
                 ))}
             </SimpleGrid>
 
-            {pricesLastModified > 0 &&
-                Date.now() - pricesLastModified >
-                    PRICE_MAP_OLD_THRESHOLD_MS && (
+            {prices.lastModified > 0 &&
+                Date.now() - prices.lastModified >
+                    PRICES_AGE_WARN_THRESHOLD_MS && (
                     <Box mt={5}>
                         <Alert status="info">
                             <AlertIcon />
                             <small>
                                 Prices last updated{" "}
-                                {timeAgo(new Date(pricesLastModified))}
+                                {timeAgo(new Date(prices.lastModified))}
                             </small>
                         </Alert>
                     </Box>
@@ -205,15 +88,14 @@ export default function PlayerPrices() {
                 <ButtonGroup colorScheme="gray" variant="solid" mt={10}>
                     <ButtonGroup isAttached>
                         <HoverTooltip
-                            label={`Fetch ${platform} market prices from ${capitalize(
-                                dataSource
-                            )}`}>
+                            label={`Fetch ${prices.externalSource.platform} market prices from ${prices.externalSource.id}`}>
                             <Button
-                                isLoading={isFetchingPrices}
+                                isLoading={prices.isFetching}
                                 loadingText="Fetching data"
-                                onClick={fetchPrices}
+                                onClick={prices.autofillExternalPrices}
                                 leftIcon={
-                                    platform === "console" ? (
+                                    prices.externalSource.platform ===
+                                    "console" ? (
                                         <Icon
                                             path={mdiGamepadVariant}
                                             size={1}
@@ -239,10 +121,17 @@ export default function PlayerPrices() {
                             </HoverTooltip>
 
                             <MenuList>
-                                <MenuOptionGroup type="radio" value={platform}>
+                                <MenuOptionGroup
+                                    type="radio"
+                                    value={prices.externalSource.platform}>
                                     <MenuItemOption
                                         value="console"
-                                        onClick={() => setPlatform("console")}>
+                                        onClick={() =>
+                                            prices.setExternalSource({
+                                                ...prices.externalSource,
+                                                platform: "console"
+                                            })
+                                        }>
                                         <HStack justifyContent="space-between">
                                             <span>Console</span>
                                             <HoverTooltip label="Console market">
@@ -254,9 +143,17 @@ export default function PlayerPrices() {
                                         </HStack>
                                     </MenuItemOption>
                                     <MenuItemOption
-                                        value="pc"
-                                        isDisabled={dataSource === "futbin"}
-                                        onClick={() => setPlatform("pc")}>
+                                        value="PC"
+                                        isDisabled={
+                                            prices.externalSource.id ===
+                                            "Futbin"
+                                        }
+                                        onClick={() =>
+                                            prices.setExternalSource({
+                                                ...prices.externalSource,
+                                                platform: "PC"
+                                            })
+                                        }>
                                         <HStack justifyContent="space-between">
                                             <span>PC</span>
                                             <HoverTooltip label="PC market">
@@ -273,16 +170,29 @@ export default function PlayerPrices() {
                                 <MenuDivider />
                                 <MenuOptionGroup
                                     type="radio"
-                                    value={dataSource}>
+                                    value={prices.externalSource.id}>
                                     <MenuItemOption
-                                        value="futwiz"
-                                        onClick={() => setDataSource("futwiz")}>
+                                        value="Futwiz"
+                                        onClick={() =>
+                                            prices.setExternalSource({
+                                                ...prices.externalSource,
+                                                id: "Futwiz"
+                                            })
+                                        }>
                                         Futwiz
                                     </MenuItemOption>
                                     <MenuItemOption
-                                        value="futbin"
-                                        isDisabled={platform === "pc"}
-                                        onClick={() => setDataSource("futbin")}>
+                                        value="Futbin"
+                                        isDisabled={
+                                            prices.externalSource.platform ===
+                                            "PC"
+                                        }
+                                        onClick={() =>
+                                            prices.setExternalSource({
+                                                ...prices.externalSource,
+                                                id: "Futbin"
+                                            })
+                                        }>
                                         Futbin
                                     </MenuItemOption>
                                 </MenuOptionGroup>
@@ -293,7 +203,7 @@ export default function PlayerPrices() {
                     <HoverTooltip label="Set all prices to 0">
                         <Button
                             leftIcon={<Icon path={mdiClose} size={0.8} />}
-                            onClick={clearAllPrices}>
+                            onClick={prices.clearAll}>
                             Reset
                         </Button>
                     </HoverTooltip>
@@ -301,69 +211,4 @@ export default function PlayerPrices() {
             </Flex>
         </>
     );
-}
-
-async function fetchExternalPrices(dataSource: DataSource, platform: Platform) {
-    const PRICE_MAX_AGE_MS = 3_600_000; // 1h
-    const GIMMICK_MS = 500; // 1h
-
-    const cacheJson = localStorage.getItem(STORAGE_KEYS.cache);
-    const cache = cacheJson
-        ? (JSON.parse(cacheJson) as {
-              [url: string]: {
-                  timestamp: number;
-                  priceMap: Record<number, number>;
-              };
-          })
-        : {};
-
-    const baseUrl = URLS[dataSource];
-    const query = new URLSearchParams();
-    if (dataSource !== "futbin") {
-        query.append("platform", platform);
-    }
-    const url = baseUrl + "?" + query;
-    if (Date.now() - cache[url]?.timestamp < PRICE_MAX_AGE_MS) {
-        await sleep(GIMMICK_MS);
-        return cache[url].priceMap;
-    }
-
-    const htmlParser = PARSERS[dataSource];
-    let cheapestByRating: Record<number, number> = {};
-    let errorMessage = "";
-    let attempts = 0;
-
-    while (attempts++ < PRICE_FETCH_MAX_ATTEMPTS) {
-        try {
-            const res = await fetch(url);
-            const html = await res.text();
-            cheapestByRating = htmlParser(html);
-
-            if (Object.keys(cheapestByRating).length === 0) {
-                throw new Error("No prices could be parsed");
-            }
-
-            errorMessage = "";
-            break;
-        } catch (error) {
-            cheapestByRating = {};
-            console.error("Could not fetch player prices: ", error);
-            errorMessage = getErrorMessage(error) || "";
-            await sleep(PRICE_FETCH_COOLDOWN_MS);
-        }
-    }
-
-    if (errorMessage) {
-        return Promise.reject(errorMessage);
-    }
-
-    if (dataSource !== "futbin") {
-        // don't cache futbin
-        cache[url] = {
-            timestamp: Date.now(),
-            priceMap: cheapestByRating
-        };
-    }
-    localStorage.setItem(STORAGE_KEYS.cache, JSON.stringify(cache));
-    return cheapestByRating;
 }
---
+++ src/hooks/usePlayerPrices.tsx
@@ -0,0 +1,253 @@
+import { EMPTY_PRICES } from "@/constants";
+import { getErrorMessage, sleep } from "@/utilities";
+import { useToast } from "@chakra-ui/react";
+import { useState } from "react";
+import { useEventTracker } from "./useEventTracker";
+import useLocalStorage from "./useLocalStorage";
+
+const PRICE_FETCH_MAX_ATTEMPTS = 3;
+const PRICE_FETCH_COOLDOWN_MS = 2000;
+const TRACKER_DEBOUNCE_MS = 3000;
+const DUMMY_DELAY_MS = 500;
+const CACHE_MAX_AGE_MS = 3_600_00; // 1h
+
+const URLS = {
+    Futbin: "https://www.futbin.com/stc/cheapest",
+    Futwiz: "/api/futwiz-cheapest"
+};
+
+const STORAGE_KEYS = {
+    externalPlatform: "prices.platform",
+    externalSource: "prices.dataSource",
+    priceMap: "prices.current",
+    cache: "prices.cache"
+};
+
+const PARSERS = {
+    Futbin: parseFutbin,
+    Futwiz: parseFutwiz
+};
+
+interface StoredPrices {
+    priceMap: { [rating: number]: number };
+    lastModified: number;
+}
+
+interface PriceProvider {
+    id: "Futbin" | "Futwiz";
+    platform: "console" | "PC";
+}
+
+function usePlayerPrices() {
+    const [storedPrices, setStoredPrices] = useLocalStorage<StoredPrices>(
+        STORAGE_KEYS.priceMap,
+        {
+            priceMap: {},
+            lastModified: -1
+        }
+    );
+    const [externalSource, setExternalSource] = useLocalStorage<PriceProvider>(
+        STORAGE_KEYS.externalSource,
+        { id: "Futwiz", platform: "console" }
+    );
+    const [isFetching, setIsFetching] = useState(false);
+
+    const eventTracker = useEventTracker("Prices", TRACKER_DEBOUNCE_MS);
+    const toast = useToast();
+
+    const autofillExternalPrices = async () => {
+        setIsFetching(true);
+        try {
+            const priceMap: Record<number, number> = await fetchExternalPrices(
+                externalSource
+            );
+            const priceCount = Object.keys(priceMap).length;
+            setAllPrices(priceMap);
+            toast({
+                status: "success",
+                description: `Fetched prices for ${priceCount} ratings`
+            });
+            eventTracker(
+                `price_fetch_ok=${externalSource.id}-${externalSource.platform}-${priceCount}`
+            );
+        } catch (error) {
+            toast({
+                status: "error",
+                title: "Could not fetch price data",
+                description: "Wait for a few minutes and try again"
+            });
+            eventTracker(
+                `price_fetch_error=${externalSource.id}-${externalSource.platform}`,
+                getErrorMessage(error)
+            );
+        } finally {
+            setIsFetching(false);
+        }
+    };
+
+    const clearAll = () => {
+        setAllPrices(EMPTY_PRICES);
+        eventTracker("price_clear_all");
+    };
+
+    const setPrice = (rating: number, price: string) => {
+        let priceNum = +price;
+        priceNum =
+            isNaN(priceNum) || typeof priceNum !== "number" ? 0 : priceNum;
+        storedPrices.priceMap[rating] = priceNum;
+        setAllPrices(storedPrices.priceMap);
+        eventTracker("price_set=" + rating + "-" + price);
+    };
+
+    const setAllPrices = (priceMap: Record<number, number>) => {
+        setStoredPrices({
+            lastModified: Date.now(),
+            priceMap: { ...EMPTY_PRICES, ...priceMap }
+        });
+    };
+
+    return {
+        priceMap: storedPrices.priceMap,
+        lastModified: storedPrices.lastModified,
+        externalSource: externalSource,
+        isFetching: isFetching,
+        setPrice: setPrice,
+        clearAll: clearAll,
+        setExternalSource: setExternalSource,
+        autofillExternalPrices: autofillExternalPrices
+    };
+}
+
+export default usePlayerPrices;
+
+async function fetchExternalPrices(priceProvider: PriceProvider) {
+    const cacheJson = localStorage.getItem(STORAGE_KEYS.cache);
+    const cache = (cacheJson ? JSON.parse(cacheJson) : {}) as {
+        [url: string]: StoredPrices;
+    };
+
+    const baseUrl = URLS[priceProvider.id];
+    const query = new URLSearchParams();
+    if (priceProvider.id !== "Futbin") {
+        query.append("platform", priceProvider.platform.toLowerCase());
+    }
+    const url = baseUrl + "?" + query;
+    if (Date.now() - cache[url]?.lastModified < CACHE_MAX_AGE_MS) {
+        await sleep(DUMMY_DELAY_MS);
+        return cache[url].priceMap;
+    }
+
+    const htmlParser = PARSERS[priceProvider.id];
+    let cheapestByRating: Record<number, number> = {};
+    let errorMessage = "";
+    let attempts = 0;
+
+    while (attempts++ < PRICE_FETCH_MAX_ATTEMPTS) {
+        try {
+            const res = await fetch(url);
+            const html = await res.text();
+            const domParser = new DOMParser();
+            const doc = domParser.parseFromString(html, "text/html");
+            cheapestByRating = htmlParser(doc);
+
+            if (Object.keys(cheapestByRating).length === 0) {
+                throw new Error("No prices could be parsed");
+            }
+
+            errorMessage = "";
+            break;
+        } catch (error) {
+            cheapestByRating = {};
+            console.warn("Could not fetch player prices: ", error);
+            errorMessage = getErrorMessage(error) || "";
+            await sleep(PRICE_FETCH_COOLDOWN_MS);
+        }
+    }
+
+    if (errorMessage) {
+        return Promise.reject(errorMessage);
+    }
+
+    if (priceProvider.id !== "Futbin") {
+        // don't cache futbin
+        cache[url] = {
+            lastModified: Date.now(),
+            priceMap: cheapestByRating
+        };
+    }
+    localStorage.setItem(STORAGE_KEYS.cache, JSON.stringify(cache));
+
+    return cheapestByRating;
+}
+
+function parseFutbin(htmlDoc: Document) {
+    const cheapestByRating: Record<number, number> = {};
+
+    const ratingGroups = htmlDoc.querySelectorAll(".top-stc-players-col");
+    ratingGroups.forEach((group) => {
+        const rating = parseInt(
+            group
+                .querySelector(".top-players-stc-title>span>span")
+                ?.innerHTML.trim() ?? "-1"
+        );
+        if (isNaN(rating) || rating === -1) {
+            return;
+        }
+        const spans = group.querySelectorAll(".price-holder-row>span");
+        const playerPrices = Array.from(spans).map((span) =>
+            parsePrice(span.textContent?.trim())
+        );
+        if (playerPrices.length === 0) {
+            return;
+        }
+
+        cheapestByRating[rating] = Math.min(...playerPrices);
+    });
+
+    return cheapestByRating;
+}
+
+function parseFutwiz(htmlDoc: Document) {
+    const cheapestByRating: Record<number, number> = {};
+
+    const ratingColumns = htmlDoc.querySelectorAll(
+        ".col-4[style='padding-right:0px;']"
+    );
+    ratingColumns.forEach((column) => {
+        const rating = parseInt(
+            column.querySelector(".title")?.innerHTML.trim() ?? "-1"
+        );
+        if (isNaN(rating) || rating === -1) {
+            return;
+        }
+        const playersBins = column.querySelectorAll(".bin");
+        const playerPrices = Array.from(playersBins).map((bin) =>
+            parsePrice(bin.textContent?.trim())
+        );
+        if (playerPrices.length === 0) {
+            return;
+        }
+
+        cheapestByRating[rating] = Math.min(...playerPrices);
+    });
+
+    return cheapestByRating;
+}
+
+function parsePrice(text: string | undefined) {
+    if (!text) {
+        return 0;
+    }
+    const isThousand = text.endsWith("K");
+    const isMillion = text.endsWith("M");
+    const num = parseFloat(text);
+    const result = isThousand ? 1000 * num : isMillion ? 1_000_000 * num : num;
+    if (
+        isNaN(result) ||
+        result === Number.POSITIVE_INFINITY ||
+        result === Number.NEGATIVE_INFINITY
+    ) {
+        throw new Error("Error parsing price '" + text + "'");
+    }
+    return result;
+}
