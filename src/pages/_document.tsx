b366440534247b8d4b658065f9da2576e2ebc380
mabenj <markobenj@hotmail.com>
2023-02-19 06:28:39 +0200

    Add adsense script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import { ColorModeScript } from "@chakra-ui/react";
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 import { theme } from "../theme";
 
 export default function Document() {
@@ -12,6 +13,12 @@ export default function Document() {
                 />
                 <Main />
                 <NextScript />
+                <Script
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3653518580503209"
+                    async
+                    strategy="beforeInteractive"
+                    crossOrigin="anonymous"
+                />
             </body>
         </Html>
     );
