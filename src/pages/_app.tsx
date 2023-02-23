1b4fb7cce459cc0392bb2c83d039478763536702
mabenj <markobenj@hotmail.com>
2023-02-23 02:34:29 +0200

    Remove ezoic and add hotjar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.scss";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+import Script from "next/script";
 import ReactGA from "react-ga";
 import { theme } from "../theme";
 
@@ -28,9 +29,23 @@ if (typeof window !== "undefined" && "navigator" in window) {
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
+            <ScriptHotjar />
             <ChakraProvider theme={theme}>
                 <Component {...pageProps} />
             </ChakraProvider>
         </>
     );
 }
+
+const ScriptHotjar = () => (
+    <Script id="hotjar">{`
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3379793,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+`}</Script>
+);
