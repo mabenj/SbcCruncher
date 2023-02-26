119c9ff66761ef77cbbe1425dcd2622975f6b89c
mabenj <markobenj@hotmail.com>
2023-02-26 23:11:35 +0200

    Minor cosmetic change to solution stats

--- src/components/form/solutions/SolutionStats.tsx
+++ src/components/form/solutions/SolutionStats.tsx
@@ -1,7 +1,5 @@
-import { useConfig } from "@/context/ConfigContext";
-import { prettyNumber, range } from "@/utilities";
+import { prettyNumber } from "@/utilities";
 import { Stat, StatHelpText, StatLabel, StatNumber } from "@chakra-ui/react";
-import { useMemo } from "react";
 
 export default function SolutionsStats({
     found,
@@ -14,13 +12,13 @@ export default function SolutionsStats({
 }) {
     return (
         <Stat mt={6}>
-            <StatLabel>Total Found</StatLabel>
-            <StatNumber>{prettyNumber(found)}</StatNumber>
-            <StatHelpText
+            <StatLabel px={2}>Total Found</StatLabel>
+            <StatNumber borderLeft="3px solid" borderColor="brand.500" px={1}>
+                {prettyNumber(found)}
+            </StatNumber>
+            <StatHelpText px={2}
                 visibility={
-                    !loading && cheapestPrice != null
-                        ? "visible"
-                        : "hidden"
+                    !loading && cheapestPrice != null ? "visible" : "hidden"
                 }>
                 <span>Cheapest {prettyNumber(cheapestPrice)} coins</span>
             </StatHelpText>
