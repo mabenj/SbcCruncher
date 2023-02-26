289e4f75342c1054642b8cfbb1e521c2f7b46ca6
mabenj <markobenj@hotmail.com>
2023-02-26 23:11:03 +0200

    Improve table-grid-toggle btn

--- src/components/form/solutions/TableGridToggle.tsx
+++ src/components/form/solutions/TableGridToggle.tsx
@@ -1,5 +1,5 @@
 import HoverTooltip from "@/components/ui/HoverTooltip";
-import { ButtonGroup, IconButton, useColorModeValue } from "@chakra-ui/react";
+import { ButtonGroup, IconButton } from "@chakra-ui/react";
 import { mdiMenu, mdiViewGrid } from "@mdi/js";
 import Icon from "@mdi/react";
 
@@ -12,23 +12,11 @@ export default function TableGridToggle({
     onSetTableView: () => void;
     onSetGridView: () => void;
 }) {
-    const activeBg = useColorModeValue("gray.200", "gray.700");
-    const nonActiveColor = useColorModeValue("gray.500", "gray.600");
-
-    const activeProps = {
-        backgroundColor: activeBg
-    };
-
-    const nonActiveProps = {
-        color: nonActiveColor,
-        variant: "ghost"
-    };
-
     return (
-        <ButtonGroup size="sm">
+        <ButtonGroup size="sm" isAttached>
             <HoverTooltip label="Table view">
                 <IconButton
-                    {...(isTableView ? activeProps : nonActiveProps)}
+                    colorScheme={isTableView ? "brand" : "gray"}
                     icon={<Icon path={mdiMenu} size={1} />}
                     aria-label="Table view"
                     onClick={onSetTableView}
@@ -36,7 +24,7 @@ export default function TableGridToggle({
             </HoverTooltip>
             <HoverTooltip label="Grid view">
                 <IconButton
-                    {...(!isTableView ? activeProps : nonActiveProps)}
+                    colorScheme={!isTableView ? "brand" : "gray"}
                     icon={<Icon path={mdiViewGrid} size={1} />}
                     aria-label="Grid view"
                     onClick={onSetGridView}
