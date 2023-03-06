c587e79b891d5bd08de81915f428bad1d2c7b027
mabenj <markobenj@hotmail.com>
2023-03-06 21:03:50 +0200

    Test scheduled background api

--- src/pages/api/background.ts
+++ src/pages/api/background.ts
@@ -1,13 +1,16 @@
-import { getErrorMessage } from "@/utilities";
+import { getErrorMessage, sleep } from "@/utilities";
 import { NextApiRequest, NextApiResponse } from "next";
 import { Log } from "./log";
-import { PriceUpdateService } from "./services/price-update.service";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
         Log.info("Starting price update");
-        const updateService = new PriceUpdateService();
-        await updateService.updateRatingPrices();
+        for (let i = 0; i < 15; i++) {
+            console.log("loop " + i);
+            await sleep(2000);
+        }
+        // const updateService = new PriceUpdateService();
+        // await updateService.updateRatingPrices();
         res.status(204).end();
         Log.info("Success");
     } catch (error) {
@@ -20,5 +23,5 @@ export default handler;
 
 export const config = {
     type: "experimental-scheduled",
-    schedule: "@hourly"
+    schedule: "* * * * *"
 };
