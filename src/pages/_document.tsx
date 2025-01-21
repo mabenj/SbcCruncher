f9ea9cda75ef77a857be368294f8fe286cc1a8c2
mabenj <markobenj@hotmail.com>
2025-01-21 19:25:45 +0200

    ExoClick test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,16 +5,27 @@ import { theme } from "../common/theme";
 // Translations disabled because of this: https://github.com/facebook/react/issues/13278
 
 export default function Document() {
-    return (
-        <Html lang="en" translate="no">
-            <Head />
-            <body className="notranslate">
-                <ColorModeScript
-                    initialColorMode={theme.config.initialColorMode}
-                />
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+  return (
+    <Html lang="en" translate="no">
+      <Head />
+      <body className="notranslate">
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+        <ExoClick />
+      </body>
+    </Html>
+  );
 }
+
+const ExoClick = () => (
+  <>
+    <script
+      async
+      type="application/javascript"
+      src="https://a.magsrv.com/ad-provider.js"
+    ></script>
+    <ins className="eas6a97888e2" data-zoneid="5521316"></ins>
+    <script>{`(AdProvider = window.AdProvider || []).push({serve: {}});`}</script>
+  </>
+);
